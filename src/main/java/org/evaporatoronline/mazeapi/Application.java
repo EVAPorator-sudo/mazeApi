29ccd2ee5e03841b45577ac9f3328067d815e9f8
evaporator <osxar.jeavons@gmail.com>
2026-02-28 20:40:09 +0000

    made application buildable

--- src/main/java/org/evaporatoronline/mazeapi/Application.java
+++ src/main/java/org/evaporatoronline/mazeapi/Application.java
@@ -3,20 +3,20 @@ package org.evaporatoronline.mazeapi;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
     }
 
-    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-        return args -> {
-            System.out.println("Application is starting!");
-        };
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
     }
 
 }
